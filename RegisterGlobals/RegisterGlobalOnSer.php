a74bf960f2aa398768ce60c9fa339b8eb551ede9
webonise <webonise@webonise-desktop.(none)>
2012-09-17 14:51:54 +0530

    error reporting,register globals

--- RegisterGlobals/RegisterGlobalOnSer.php
+++ RegisterGlobals/RegisterGlobalOnSer.php
@@ -1,5 +1,5 @@
 <?php
- 
+  // ini_set('register_globals', 'on');
  ini_set('error_reporting', E_ALL);
 ini_set('display_errors',1);
    $user= $_GET['username'];
---
+++ errorReporting/errorReprtingOn.php
@@ -0,0 +1,7 @@
+<?php
+
+ini_set('display_errors','On'); 
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+$fh = fopen('hello.txt', 'w');
+
+?>
